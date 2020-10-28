465c6481ec3b3d18bb74c7421b24c12078f7d982
jeff <jeffrey.spiers@mail.mcgill.ca>
2020-10-28 16:15:04 -0600

    g1-small

--- gke.tf
+++ gke.tf
@@ -32,7 +32,7 @@ resource "google_container_node_pool" "engineering_preemptible_nodes" {
   node_config {
     preemptible  = true
     # machine_type = "n1-standard-1"
-    machine_type = "g1-micro" # "f1-micro" free tier not allowed (too little memory)
+    machine_type = "g1-small" # "f1-micro" free tier not allowed (too little memory)
 
     metadata = {
       disable-legacy-endpoints = "true"
