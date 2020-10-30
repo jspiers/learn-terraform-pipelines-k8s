19abfca7856272b30cdd7f08e11269b70991bf55
jeff <jeffrey.spiers@mail.mcgill.ca>
2020-10-30 17:14:51 -0600

    removing gke variables

--- variables.tf
+++ variables.tf
@@ -10,22 +10,6 @@ variable "cluster_name" {
   description = "Name of cluster."
 }
 
-variable "google_project" {
-  type        = string
-  description = "Google Project to deploy cluster"
-}
-
-variable "username" {
-  type        = string
-  default     = "admin"
-  description = "Username for GKE clusters"
-}
-
-variable "password" {
-  type        = string
-  description = "Password for GKE clusters"
-}
-
 variable "enable_consul_and_vault" {
   type        = bool
   default     = false
