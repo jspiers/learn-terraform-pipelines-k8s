56f152eaa1d2d3eaca3daf152e0717108b5df8f5
jeff <jeffrey.spiers@mail.mcgill.ca>
2020-10-30 17:28:36 -0600

    removing unnecessary outputs

--- outputs.tf
+++ outputs.tf
@@ -7,25 +7,15 @@ output "version" {
   value = digitalocean_kubernetes_cluster.engineering.version
 }
 
-output "host" {
-  # value     = google_container_cluster.engineering.endpoint
-  value     = digitalocean_kubernetes_cluster.engineering.endpoint
-  sensitive = true
-}
-
-output "cluster_ca_certificate" {
-  # value     = base64decode(google_container_cluster.engineering.master_auth.0.cluster_ca_certificate)
-  value     = base64decode(digitalocean_kubernetes_cluster.engineering.kube_config[0].cluster_ca_certificate)
-  sensitive = true
-}
-
-# output "username" {
-#   value     = google_container_cluster.engineering.master_auth.0.username
+# output "host" {
+#   # value     = google_container_cluster.engineering.endpoint
+#   value     = digitalocean_kubernetes_cluster.engineering.endpoint
 #   sensitive = true
 # }
 
-# output "password" {
-#   value     = google_container_cluster.engineering.master_auth.0.password
+# output "cluster_ca_certificate" {
+#   # value     = base64decode(google_container_cluster.engineering.master_auth.0.cluster_ca_certificate)
+#   value     = base64decode(digitalocean_kubernetes_cluster.engineering.kube_config[0].cluster_ca_certificate)
 #   sensitive = true
 # }
 
